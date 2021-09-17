6bf292695bac3e6fcdaf01688edf9946d39a8db1
AJ Rice <53190766+ajrice6713@users.noreply.github.com>
2021-09-17 13:18:11 -0400

    Add Test and Exports

--- tests/api.test.js
+++ tests/api.test.js
@@ -1,4 +1,4 @@
-import { ApiError, ApiController, Client } from '../src';
+import { ApiError, ApiController, Client, ModeEnum, CallbackMethodEnum, MachineDetectionRequest } from '../src';
 
 let controller;
 
@@ -30,7 +30,48 @@ describe('api', () => {
         expect(createCallResponse.result.applicationId).toEqual(applicationId);
         expect(createCallResponse.result.to).toEqual(to);
         expect(createCallResponse.result.from).toEqual(from);
-        
+
+        // get call state
+        const callId = createCallResponse.result.callId;
+        const getCallStateResponse = await controller.getCall(accountId, callId);
+
+        expect(getCallStateResponse.result.applicationId).toEqual(applicationId);
+        expect(getCallStateResponse.result.to).toEqual(to);
+        expect(getCallStateResponse.result.from).toEqual(from);
+        expect(getCallStateResponse.result.callId).toEqual(callId);
+    });
+
+    it('should create call with AMD and get call state', async () => {
+        // create call
+        const accountId = process.env.BW_ACCOUNT_ID;
+        const from = process.env.BW_NUMBER;
+        const to = process.env.USER_NUMBER;
+        const answerUrl = `${process.env.BASE_CALLBACK_URL}/callbacks/answer`;
+        const applicationId = process.env.BW_VOICE_APPLICATION_ID;
+        const machineDetection = {
+            mode: ModeEnum.Async,
+            detectionTimeout: 5.0,
+            silenceTimeout: 5.0,
+            speechThreshold: 5.0,
+            speechEndThreshold: 5.0,
+            delayResult: true,
+            callbackUrl: `${process.env.BASE_CALLBACK_URL}/callbacks/machineDetection`,
+            callbackMethod: CallbackMethodEnum.POST,
+        };
+
+        const body = {
+          from: from,
+          to: to,
+          answerUrl: answerUrl,
+          applicationId: applicationId,
+          machineDetection: machineDetection
+        };
+
+        const createCallResponse = await controller.createCall(accountId, body);
+        expect(createCallResponse.result.applicationId).toEqual(applicationId);
+        expect(createCallResponse.result.to).toEqual(to);
+        expect(createCallResponse.result.from).toEqual(from);
+
         // get call state
         const callId = createCallResponse.result.callId;
         const getCallStateResponse = await controller.getCall(accountId, callId);
